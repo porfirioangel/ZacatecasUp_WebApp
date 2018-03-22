d37b57ec60df7dac46f967b58b61b9bf389ae1fa
stylder <Santiago1>
2018-03-22 02:19:50 -0600

    Ya no se muestra el error de actualizar el  negocio

--- src/app/content/negocios/modal-negocio/mapa/mapa.component.ts
+++ src/app/content/negocios/modal-negocio/mapa/mapa.component.ts
@@ -1,36 +1,26 @@
+import { FormGroup } from '@angular/forms';
 import { Input } from '@angular/core';
 import { Negocio } from './../../../../clases/negocio';
 import { log } from 'util';
-import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
-import { AgmMap } from '@agm/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-ms-mapa-negocio',
   templateUrl: './mapa.component.html',
   styleUrls: ['./mapa.component.scss']
 })
-export class MapaComponent implements OnInit,  AfterViewInit {
+export class MapaComponent implements OnInit {
 
-  title = 'My first AGM project';
-  lat = 22.7633237;
-  lng = -102.5966308;
-  @ViewChild('agmMap') agmMap;
 
   @Input('item') item: Negocio;
   @Input('isNew') isNew: boolean;
+  @Input('form') form: FormGroup;
+
 
   constructor() {
   }
 
   ngOnInit() {
-    // this.lat = Number(this.item.latitud)
-    // this.lng = Number(this.item.longitud)
-  }
-
-  ngAfterViewInit() {
-    console.log('ngAfterViewInit');
-    console.log(this.agmMap);
-    this.agmMap.triggerResize();
   }
 
 }
